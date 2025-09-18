46b1934e34a7c38f95da7d7c6904d899f91f7c16
Jake Soenneker <jake@soenneker.com>
2025-09-18 08:59:31 -0500

    Initial commit

---
+++ src/Registrars/OffcanvasRegistrar.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Soenneker.Blazor.Utils.ResourceLoader.Registrars;
+using Soenneker.Quark.Offcanvases.Abstract;
+
+namespace Soenneker.Quark.Offcanvases.Registrars;
+
+/// <summary>
+/// A Blazor Quark component for OffcanvasInterop.
+/// </summary>
+public static class OffcanvasRegistrar
+{
+    /// <summary>
+    /// Adds <see cref="IOffcanvasInterop"/> as a scoped service. <para/>
+    /// </summary>
+    public static IServiceCollection AddOffcanvasAsScoped(this IServiceCollection services)
+    {
+        services.AddResourceLoaderAsScoped()
+            .TryAddScoped<IOffcanvasInterop, OffcanvasInterop>();
+
+        return services;
+    }
+}
---
+++ test/Soenneker.Quark.Offcanvases.Tests/OffcanvasInteropInteropTests.cs
@@ -0,0 +1,22 @@
+﻿using Soenneker.Quark.Offcanvases.Abstract;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Quark.Offcanvases.Tests;
+
+[Collection("Collection")]
+public sealed class OffcanvasInteropInteropTests : FixturedUnitTest
+{
+    private readonly IOffcanvasInterop _blazorlibrary;
+
+    public OffcanvasInteropInteropTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _blazorlibrary = Resolve<IOffcanvasInterop>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
